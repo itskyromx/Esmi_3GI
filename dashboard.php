fe3642d31c2b2b284da0cb7c3c874c9c92a2eda7
itskyromx <31516509+itskyromx@users.noreply.github.com>
2025-01-12 23:50:40 +0000

    4th COMMIT & PUSH

--- dashboard.php
+++ dashboard.php
@@ -5,8 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Account | Stackz Clothing</title>
     <link rel="icon" type="image\x-icon" href="icons/favicon.ico">
+    <link rel="stylesheet" href="css/styles.css"/>
 </head>
 <body>
+    <header>
+        <class="container">
+            <nav>
+                <ul>
+                <a href=<?php echo isset($_SESSION['id']) ? 'dashboard.php' : 'login.php'; ?>>
+                    <img src="icons/login_icon.png" alt="Login" class="login">
+                </a>
+                </ul>
+                
+                <ul>
+                <a href="#">
+                    <img src="icons/search_icon.png" alt="Search" class="search">
+                </a>
+                </ul>
+
+                <ul>
+                <a href="#">
+                    <img src="icons/cart_icon.png" alt="Cart" class="cart">
+                </a>
+                </ul>
+            </nav>
+            <a href="index.php">
+                <img src="icons/stackz_logo.png" alt="Logo" class="logo">
+            </a>
+        </div>
+    </header>
     <h2>
     <a href="logout.php">LOGOUT</a>
     </h2>
